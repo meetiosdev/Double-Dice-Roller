989e26f84778f600e2375615313c0c55a78899d2
Meet Mali <meetmalidev@gmail.com>
2022-01-01 23:14:10 +0530

    Initial Commit

---
+++ Double-Dice-Roller/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Double-Dice-Roller
+///////////////////////////////////////////////////
+//  Created by Meet Mali
+//  Follow @meetiosdev
+//  Date: 01/Jan/22
+///////////////////////////////////////////////////
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+   
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+ 
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+    }
+    
+}
